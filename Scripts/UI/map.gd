79291cd4063928281921344a279bf65b66367525
Kopelat <57046428+Kopelat@users.noreply.github.com>
2026-08-24 13:39:35 +0200

    updated map

--- Scripts/UI/map.gd
+++ Scripts/UI/map.gd
@@ -1,16 +1,51 @@
 extends Control
 
+const SCROLL_SPEED := 0.1
+
+var node_position := Vector2.ZERO
+
 var dragging := false
+var scaled_up := false
+
 var tap_position := Vector2.ZERO
+var map_position := Vector2.ZERO
+var map_scale := 2.0
+
+func _ready() -> void:
+	node_position = position
+	update_map_size()
 
 func _process(delta: float) -> void:
 	if dragging:
-		$MapWorld.position = tap_position - get_global_mouse_position()
+		$MapWorld.position = map_position + -(tap_position - get_global_mouse_position())
 
-func _on_map_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.is_pressed():
-		print("pressed")
-		dragging = true
-		tap_position = get_global_mouse_position()
+func _on_button_button_down() -> void:
+	dragging = true
+	tap_position = get_global_mouse_position()
+	map_position = $MapWorld.position
+
+func _on_button_button_up() -> void:
+	dragging = false
+
+func _on_button_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
+			map_scale = clampf(map_scale + SCROLL_SPEED * map_scale, 4.0, 16.0)
+			update_map_size()
+		
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
+			map_scale = clampf(map_scale - SCROLL_SPEED * map_scale, 4.0, 16.0)
+			update_map_size()
+
+func update_map_size():
+	$MapWorld.scale = Vector2(map_scale, map_scale)
+
+func _on_scale_button_button_down() -> void:
+	scaled_up = not scaled_up
+	
+	if scaled_up:
+		size = Vector2(1100, 580)
+		position = node_position - Vector2(size.x - 200, 0)
 	else:
-		dragging = false
+		size = Vector2(200, 200)
+		position = node_position
