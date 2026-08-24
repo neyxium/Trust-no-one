165b174d1b70b6ae33ebff22b987042f9bdb6a4d
Neyxium <50521342+neyxium@users.noreply.github.com>
2026-08-24 14:13:49 +0200

    Random calls + new templates

--- Scenes/Core/game_manager.gd
+++ Scenes/Core/game_manager.gd
@@ -1,14 +1,12 @@
 extends Control
 
-@export var current_call: EmergencyCall
-
 @onready var dialogue_label: RichTextLabel = %Dialogue
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if current_call != null:
-		Show_call(current_call)
-	else:
-		dialogue_label.text = "Waiting for call..."
+	randomize()
+	
+	var current_call = EmergencyCall.generate_random_call()
+	Show_call(current_call)
 
 func Show_call(call: EmergencyCall) -> void:
 	dialogue_label.text = call.get_formatted_dialogue()
--- Scripts/EmergencyCall.gd
+++ Scripts/EmergencyCall.gd
@@ -46,13 +46,13 @@ enum IncidentType
 	Robbery,
 	Burglary,
 	Fire,
-	GasLeak,
+	Gas_Leak,
 	Medical_Emergency,
 	Assault,
 	Vandalism,
-	SuspiciousActivity,
-	SuspiciousPerson,
-	MissingPerson
+	Suspicious_Activity,
+	Suspicious_Person,
+	Missing_Person
 }
 
 @export var call_id: String = "Call_001"
@@ -65,7 +65,18 @@ enum IncidentType
 @export var caller_tellin_truth: bool
 
 @export_group("Templates")
-@export_multiline var text: String = "Hello, I am {caller} and I am at {location}. Here happened {incident}."
+@export_multiline var text: String
+const DIALOGUE_TEMPLATES: Array[String] = [
+	"Oh god, please help me! My name is {caller}. I'm currently at {location} and... oh no, there's an {incident}! Please send someone immediately!",
+	"Hi, yes, dispatch? This is {caller}. Listen, I'm standing right outside {location} and a major {incident} just broke out. I think someone is hurt!",
+	"Emergency services, please! My name is {caller}, calling from {location}. We have a severe {incident} happening right in front of me. You need to hurry up!",
+	"Yeah, hi, um... {caller} here. Look, something weird is going on at {location}. It looks kind of like a {incident} or something? Honestly, I'm not really sure, just maybe send a car?",
+	"Hello? Is this the emergency line? My name is {caller} and... look, don't ask questions, but at {location} there's an {incident} and I really need you to clear out. Quickly.",
+	"Hey dispatcher, {caller} here. Just letting you know, somebody messed up big time over at {location}. Total {incident}. You guys should probably check that out... or not, whatever.",
+	"Ugh, hello? This is {caller}. I was just trying to enjoy my day at {location} and now there's a complete {incident} blocking my way. This is completely unacceptable!",
+	"Listen to me very carefully, my name is {caller}. You need to send units to {location} right now because this {incident} is ruining everything. Do you know who I am?!",
+	"Uh, hello, 911? {caller} speaking from {location}. So, funny story... a massive {incident} just happened out of nowhere. Can you like, send someone? Thanks."
+]
 
 func get_formatted_dialogue() -> String:
 	var name_str = get_caller_name()
@@ -97,3 +108,14 @@ func get_caller_location() -> String:
 func get_caller_incident() -> String:
 	var choice: IncidentType = _get_truth_or_lie_value(caller_incident, IncidentType.values(), caller_tellin_truth)
 	return IncidentType.keys()[choice].replace("_", " ")
+	
+static func generate_random_call() -> EmergencyCall:
+	var random_call = EmergencyCall.new()
+	
+	random_call.caller_name = CallerName.values().pick_random() as CallerName
+	random_call.caller_location = LocationName.values().pick_random() as LocationName
+	random_call.caller_incident = IncidentType.values().pick_random() as IncidentType
+	random_call.caller_tellin_truth = randf() < 0.5
+	random_call.text = DIALOGUE_TEMPLATES.pick_random()
+	
+	return random_call
