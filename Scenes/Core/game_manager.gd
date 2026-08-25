cb489fe486d35f4d8ef1e4c556ef3399f36d3391
Kopelat <57046428+Kopelat@users.noreply.github.com>
2026-08-25 16:02:57 +0200

    addeded the dialog animation thingy back

--- Scenes/Core/game_manager.gd
+++ Scenes/Core/game_manager.gd
@@ -7,6 +7,8 @@ extends Control
 var current_call: EmergencyCall
 var available_units: int = 3
 
+var call_running := false
+
 var successful_responses: int = 0
 var wasted_units: int = 0
 var unanswered_crises: int = 0
@@ -19,13 +21,38 @@ func _ready() -> void:
 
 	start_new_call()
 
+func show_call(call: EmergencyCall) -> void:
+	for num in range(call.dialogue_list.size()):
+		if not call_running: # if call ended before finishing
+			return
+		
+		dialogue_label.visible_characters = 0
+		dialogue_label.text = call.get_formatted_dialogue(num)
+		
+		while dialogue_label.visible_characters < dialogue_label.text.length() and call_running:
+			dialogue_label.visible_characters += 1
+			for i in range(4):
+				if not call_running: # if call ended before finishing
+					return
+				
+				await get_tree().process_frame
+		
+		for i in range(120): # 2 sec
+			if not call_running: # if call ended before finishing
+				return
+			
+			await get_tree().process_frame
+
 func start_new_call() -> void:
-	current_call = EmergencyCall.generate_random_call()
-	dialogue_label.text = current_call.get_formatted_dialogue(0)
+	print("Razpoložljive enote: ", available_units)
 	
 	send_button.disabled = (available_units <= 0)
+	current_call = EmergencyCall.generate_random_call()
 	
-	print("Razpoložljive enote: ", available_units)
+	await get_tree().create_timer(1).timeout
+	
+	call_running = true
+	show_call(current_call)
 
 func _on_send_units_pressed() -> void:
 	if available_units > 0:
@@ -36,12 +63,18 @@ func _on_send_units_pressed() -> void:
 		else:
 			wasted_units += 1
 		
-		start_new_call()
+		call_running = false
+		dialogue_label.visible_characters = 0
 		
+		start_new_call()
+
 func _on_finish_call_pressed() -> void:
 	if current_call.caller_tellin_truth:
 		unanswered_crises += 1
 	else:
 		successful_responses += 1
-		
+	
+	call_running = false
+	dialogue_label.visible_characters = 0
+	
 	start_new_call()
