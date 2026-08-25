b4fd48ac646246388f8764a4992eb45eb8a648e5
Neyxium <50521342+neyxium@users.noreply.github.com>
2026-08-25 14:41:51 +0200

    send units or end call

--- Scenes/Core/game_manager.gd
+++ Scenes/Core/game_manager.gd
@@ -1,21 +1,47 @@
 extends Control
 
 @onready var dialogue_label: RichTextLabel = %Dialogue
-# Called when the node enters the scene tree for the first time.
+@onready var send_button: Button = %SendUnitsButton
+@onready var finish_button: Button = %FinishCallButton
+
+var current_call: EmergencyCall
+var available_units: int = 3
+
+var successful_responses: int = 0
+var wasted_units: int = 0
+var unanswered_crises: int = 0
+
 func _ready() -> void:
 	randomize()
 	
-	var current_call = EmergencyCall.generate_random_call()
-	Show_call(current_call)
+	send_button.pressed.connect(_on_send_units_pressed)
+	finish_button.pressed.connect(_on_finish_call_pressed)
 
-func Show_call(call: EmergencyCall) -> void:
-	for num in range(call.dialogue_list.size()):
-		dialogue_label.visible_characters = 0
-		dialogue_label.text = call.get_formatted_dialogue(num)
+	start_new_call()
+
+func start_new_call() -> void:
+	current_call = EmergencyCall.generate_random_call()
+	dialogue_label.text = current_call.get_formatted_dialogue(0)
+	
+	send_button.disabled = (available_units <= 0)
+	
+	print("Razpoložljive enote: ", available_units)
+
+func _on_send_units_pressed() -> void:
+	if available_units > 0:
+		available_units -= 1
 		
-		while dialogue_label.visible_characters < dialogue_label.text.length():
-			dialogue_label.visible_characters += 1
-			for i in range(4):
-				await get_tree().process_frame
+		if current_call.caller_tellin_truth:
+			successful_responses += 1
+		else:
+			wasted_units += 1
 		
-		await get_tree().create_timer(2).timeout
+		start_new_call()
+		
+func _on_finish_call_pressed() -> void:
+	if current_call.caller_tellin_truth:
+		unanswered_crises += 1
+	else:
+		successful_responses += 1
+		
+	start_new_call()
